a7388fed6218b27190bb6f6efc8dd03cac884019
MarinPuizina <marinpuizina@hotmail.com>
2019-06-07 00:08:35 +0200

    Added Exception handling classes with  custom messages.

--- src/main/java/app/ws/ui/controller/UserController.java
+++ src/main/java/app/ws/ui/controller/UserController.java
@@ -1,8 +1,10 @@
 package app.ws.ui.controller;
 
+import app.ws.exceptions.UserServiceException;
 import app.ws.service.UserService;
 import app.ws.shared.dto.UserDto;
 import app.ws.ui.model.request.UserDetailsRequestModel;
+import app.ws.ui.model.response.ErrorMessages;
 import app.ws.ui.model.response.UserRest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,9 +35,11 @@ public class UserController {
                              MediaType.APPLICATION_JSON_VALUE},
             produces = {MediaType.APPLICATION_XML_VALUE,
                         MediaType.APPLICATION_JSON_VALUE})
-    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) throws UserServiceException {
 
         UserRest returnValue = new UserRest();
+
+        if(userDetails.getFirstName().isEmpty()) throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
 
         UserDto userDto = new UserDto();
         BeanUtils.copyProperties(userDetails, userDto); // Populating userDto with information that came with Request body
