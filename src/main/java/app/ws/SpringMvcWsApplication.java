d220771b152e9b6c4da7a91d22c199fd4eab6e57
MarinPuizina <marinpuizina@hotmail.com>
2019-06-08 01:36:02 +0200

    Made project deployable as war file

--- src/main/java/app/ws/SpringMvcWsApplication.java
+++ src/main/java/app/ws/SpringMvcWsApplication.java
@@ -3,11 +3,19 @@ package app.ws;
 import app.ws.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+// SpringBootServletInitializer to be able to make war file
 @SpringBootApplication
-public class SpringMvcWsApplication {
+public class SpringMvcWsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringMvcWsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcWsApplication.class, args);
