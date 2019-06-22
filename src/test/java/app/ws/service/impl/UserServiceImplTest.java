dbb9ef78dd9b2a570347fe9bf690b35f5dd548af
MarinPuizina <marinpuizina@hotmail.com>
2019-06-22 02:27:03 +0200

    Added more test cases inside UserServicesImplTest - createUser test method.

--- src/test/java/app/ws/service/impl/UserServiceImplTest.java
+++ src/test/java/app/ws/service/impl/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package app.ws.service.impl;
 
+import app.ws.io.entity.AddressEntity;
 import app.ws.io.entity.UserEntity;
 import app.ws.io.repository.UserRepository;
 import app.ws.shared.Utils;
@@ -10,15 +11,18 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class UserServiceImplTest {
 
@@ -46,10 +50,12 @@ class UserServiceImplTest {
         userEntity = new UserEntity();
         userEntity.setId(1L);
         userEntity.setFirstName("Marin");
+        userEntity.setLastName("Puizina");
         userEntity.setUserId(userId);
         userEntity.setEncryptedPassword(encryptedPassword);
         userEntity.setEmail("test@test.com");
         userEntity.setEmailVerificationToken("sdawadsda");
+        userEntity.setAddresses(getAddressesEntity());
 
     }
 
@@ -91,19 +97,58 @@ class UserServiceImplTest {
 
         when(userRepository.save(any(UserEntity.class))).thenReturn(userEntity);
 
-        AddressDto addressDto = new AddressDto();
-        addressDto.setType("shipping");
-
-        List<AddressDto> addresses = new ArrayList<>();
-        addresses.add(addressDto);
 
         UserDto userDto = new UserDto();
-        userDto.setAddresses(addresses);
+        userDto.setAddresses(getAddressesDto());
+        userDto.setFirstName("Marin");
+        userDto.setLastName("Puizina");
+        userDto.setPassword("123");
+        userDto.setEmail("test@test.com");
 
         UserDto storedUserDetails = userService.createUser(userDto);
         assertNotNull(storedUserDetails);
 
         assertEquals(userEntity.getFirstName(), storedUserDetails.getFirstName());
+        assertEquals(userEntity.getLastName(), storedUserDetails.getLastName());
+        assertNotNull(storedUserDetails.getUserId());
+        assertEquals(storedUserDetails.getAddresses().size(), userEntity.getAddresses().size());
+        verify(utils, times(2)).generateAddressId(30);
+        verify(bCryptPasswordEncoder, times(1)).encode("123");
+        verify(userRepository, times(1)).save(any(UserEntity.class))    ;
+
+    }
+
+    private List<AddressDto> getAddressesDto() {
+
+        AddressDto addressDto = new AddressDto();
+        addressDto.setType("shipping");
+        addressDto.setCity("Split");
+        addressDto.setCountry("Croatia");
+        addressDto.setPostalCode("21000");
+        addressDto.setStreetName("123 street name");
+
+        AddressDto billingAddressDto = new AddressDto();
+        billingAddressDto.setType("billing");
+        billingAddressDto.setCity("Split");
+        billingAddressDto.setCountry("Croatia");
+        billingAddressDto.setPostalCode("21000");
+        billingAddressDto.setStreetName("123 street name");
+
+        List<AddressDto> addresses = new ArrayList<>();
+        addresses.add(addressDto);
+        addresses.add(billingAddressDto);
+
+        return addresses;
+
+    }
+
+    private List<AddressEntity> getAddressesEntity() {
+
+        List<AddressDto> addresses = getAddressesDto();
+
+        Type listType = new TypeToken<List<AddressEntity>>() {}.getType();
+
+        return new ModelMapper().map(addresses, listType);
 
     }
 
