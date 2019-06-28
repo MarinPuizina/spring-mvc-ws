257d0d804f0610205949affe469da12394ac5725
MarinPuizina <marinpuizina@hotmail.com>
2019-06-28 23:50:42 +0200

    Added native sql method to find users with validated emails, also added test case for that method.

--- src/main/java/app/ws/io/repository/UserRepository.java
+++ src/main/java/app/ws/io/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package app.ws.io.repository;
 
 import app.ws.io.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +17,14 @@ public interface UserRepository extends PagingAndSortingRepository<UserEntity, L
     UserEntity findByUserId(String userId);
 
     UserEntity findUserByEmailVerificationToken(String token);
+
+    // Important - for query to be treated as native SQL query, we need to provide a value and "nativeQuery = true"
+    // Also, we use custom name for calling this query
+    // countQuery is used by JPA to figure total amount of records that needs to split in different pages
+    // If we don't provide the countQuery then JPA will try to figure out the count of records by itself
+    @Query(value = "select * from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'",
+            countQuery = "select count(*) from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'",
+            nativeQuery = true)
+    Page<UserEntity> findAllUsersWithConfirmedEmailAddress(Pageable pageableRequest);
 
 }
---
+++ src/test/java/app/ws/io/repository/UserRepositoryTest.java
@@ -0,0 +1,39 @@
+package app.ws.io.repository;
+
+import app.ws.io.entity.UserEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//Needed to make it an integration test
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAllUsersWithConfirmedEmailAddress() {
+
+        Pageable pageableRequest = PageRequest.of(0, 2);
+        Page<UserEntity> pages = userRepository.findAllUsersWithConfirmedEmailAddress(pageableRequest);
+
+        assertNotNull(pages);
+
+    }
+
+}
