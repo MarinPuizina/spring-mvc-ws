13ea6d623abd6a224560dee616022757be598edf
MarinPuizina <marinpuizina@hotmail.com>
2019-06-29 00:19:27 +0200

    Added dummy data to fill database if our test db has no recordsto be able to test findAllUsersWithConfirmedEmailAddress

--- src/test/java/app/ws/io/repository/UserRepositoryTest.java
+++ src/test/java/app/ws/io/repository/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package app.ws.io.repository;
 
+import app.ws.io.entity.AddressEntity;
 import app.ws.io.entity.UserEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,6 +28,62 @@ class UserRepositoryTest {
 
     @BeforeEach
     void setUp() {
+
+        /*
+        // Prepare User Entity
+        UserEntity userEntity = new UserEntity();
+        userEntity.setFirstName("Marin");
+        userEntity.setLastName("Puizina");
+        userEntity.setUserId("1a2b3c");
+        userEntity.setEncryptedPassword("123");
+        userEntity.setEmail("test@test.com");
+        userEntity.setEmailVerificationStatus(true);
+
+        // Prepare User Addresses
+        AddressEntity addressEntity = new AddressEntity();
+        addressEntity.setType("shipping");
+        addressEntity.setAddressId("ahgyt74hfy");
+        addressEntity.setCity("Split");
+        addressEntity.setCountry("Croatia");
+        addressEntity.setPostalCode("ABCCDA");
+        addressEntity.setStreetName("123 Street Address");
+
+        List<AddressEntity> addresses = new ArrayList<>();
+        addresses.add(addressEntity);
+
+        userEntity.setAddresses(addresses);
+
+        userRepository.save(userEntity);
+
+
+
+
+        // Prepare User Entity
+        UserEntity userEntity2 = new UserEntity();
+        userEntity2.setFirstName("Marin");
+        userEntity2.setLastName("Puizina");
+        userEntity2.setUserId("1a2b3cddddd");
+        userEntity2.setEncryptedPassword("123");
+        userEntity2.setEmail("test2@test.com");
+        userEntity2.setEmailVerificationStatus(true);
+
+        // Prepare User Addresses
+        AddressEntity addressEntity2 = new AddressEntity();
+        addressEntity2.setType("shipping");
+        addressEntity2.setAddressId("ahgyt74hfywwww");
+        addressEntity2.setCity("Split");
+        addressEntity2.setCountry("Croatia");
+        addressEntity2.setPostalCode("ABCCDA");
+        addressEntity2.setStreetName("123 Street Address");
+
+        List<AddressEntity> addresses2 = new ArrayList<>();
+        addresses2.add(addressEntity2);
+
+        userEntity2.setAddresses(addresses2);
+
+        userRepository.save(userEntity2);
+        */
+
     }
 
     @Test
